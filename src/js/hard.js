987e77c3dfda24052cb8851c8990a4dd065fa901
Jack Storrs <jackstorrs8@gmail.com>
2016-11-03 14:37:03 -0400

    Commented out the entire section of the crappy 'tick' method and starting from square one.

--- src/js/hard.js
+++ src/js/hard.js
@@ -5,61 +5,75 @@ class GildedRose {
     this.quality = quality;
   }
 
-  tick () {
-    if (!this.name.includes("Conjured")){
-      if (this.name != 'Aged Brie' && this.name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.quality > 0) {
-          if (this.name != 'Sulfuras, Hand of Ragnaros') {
-            this.quality = this.quality - 1;
-          }
-        }
-      } else {
-        if (this.quality < 50) {
-          this.quality = this.quality + 1;
-          if (this.name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.sellIn < 11) {
-              if (this.quality < 50) {
-                this.quality = this.quality + 1;
-              }
-            }
-            if (this.sellIn < 6) {
-              if (this.quality < 50) {
-                this.quality = this.quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.name != 'Sulfuras, Hand of Ragnaros') {
-        this.sellIn = this.sellIn - 1;
-      }
-      if (this.sellIn < 0) {
-        if (this.name != 'Aged Brie') {
-          if (this.name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.quality > 0) {
-              if (this.name != 'Sulfuras, Hand of Ragnaros') {
-                this.quality = this.quality - 1;
-              }
-            }
-          } else {
-            this.quality = this.quality - this.quality;
-          }
-        } else {
-          if (this.quality < 50) {
-            this.quality = this.quality + 1;
-          }
-        }
-      }
-    } else {
-        this.sellIn = this.sellIn - 1;
-        if (this.sellIn >= 0 ) {
-          this.quality = this.quality - 2;
-        } else {this.quality = this.quality - 4}
-        if (this.quality < 0 ) {
-          this.quality = 0;
-        }
-    }
-  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// tick () {
+//   if (!this.name.includes("Conjured")){
+//     if (this.name != 'Aged Brie' && this.name != 'Backstage passes to a TAFKAL80ETC concert') {
+//       if (this.quality > 0) {
+//         if (this.name != 'Sulfuras, Hand of Ragnaros') {
+//           this.quality = this.quality - 1;
+//         }
+//       }
+//     } else {
+//       if (this.quality < 50) {
+//         this.quality = this.quality + 1;
+//         if (this.name == 'Backstage passes to a TAFKAL80ETC concert') {
+//           if (this.sellIn < 11) {
+//             if (this.quality < 50) {
+//               this.quality = this.quality + 1;
+//             }
+//           }
+//           if (this.sellIn < 6) {
+//             if (this.quality < 50) {
+//               this.quality = this.quality + 1;
+//             }
+//           }
+//         }
+//       }
+//     }
+//     if (this.name != 'Sulfuras, Hand of Ragnaros') {
+//       this.sellIn = this.sellIn - 1;
+//     }
+//     if (this.sellIn < 0) {
+//       if (this.name != 'Aged Brie') {
+//         if (this.name != 'Backstage passes to a TAFKAL80ETC concert') {
+//           if (this.quality > 0) {
+//             if (this.name != 'Sulfuras, Hand of Ragnaros') {
+//               this.quality = this.quality - 1;
+//             }
+//           }
+//         } else {
+//           this.quality = this.quality - this.quality;
+//         }
+//       } else {
+//         if (this.quality < 50) {
+//           this.quality = this.quality + 1;
+//         }
+//       }
+//     }
+//   } else {
+//       this.sellIn = this.sellIn - 1;
+//       if (this.sellIn >= 0 ) {
+//         this.quality = this.quality - 2;
+//       } else {this.quality = this.quality - 4}
+//       if (this.quality < 0 ) {
+//         this.quality = 0;
+//       }
+//   }
+// }
 
 export { GildedRose };
