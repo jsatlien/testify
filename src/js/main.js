dac3b57d567ef7bac4f592df7682c37a762c6d77
Jack Storrs <jackstorrs8@gmail.com>
2016-11-03 12:01:17 -0400

    final fizz test is passing.

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 function fizz (num) {
-  if (num % 3 === 0 ) {
-    return "fizz";
+  if (num % 5 === 0 && num % 3 === 0) {
+    return "fizzbuzz";
   } else if (num % 5 === 0) {
       return "buzz";
+  } else if (num % 3 === 0 ) {
+    return "fizz";
   } else {
       return num;
   };
