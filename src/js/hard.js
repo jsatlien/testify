742cd87e015d36cf825a557ad65c9c1c53f92c97
Jack Storrs <jackstorrs8@gmail.com>
2016-11-03 23:10:03 -0400

    Finished writing method for tickets, on to testing.

--- src/js/hard.js
+++ src/js/hard.js
@@ -20,15 +20,15 @@ class GildedRose {
     // addType(this);
 
     if (this.type === 'aged') {
-        this.tickAged()
+        this.tickAged();
     } else if (this.type === 'conjured') {
         console.log('conjured')
     } else if (this.type === 'legendary') {
         console.log('legendary')
     } else if (this.type === 'ticket') {
-        console.log('Rock on dude!')
+        this.tickTicket();
     } else {
-        this.tickNormal()// this.type = 'normal'
+        this.tickNormal();// this.type = 'normal'
     }
   };
   tickNormal() {
@@ -47,9 +47,22 @@ class GildedRose {
     this.quality++;
     if (this.sellIn < 0) {
       this.quality++;
+      // ask brit or ben why it was necessary to include this if statement to make the tests pass.
     }
     if (this.quality > 50) {
       this.quality = 50;
+    }
+  };
+  tickTicket() {
+    this.sellIn--;
+    if (this.sellIn > 10) {
+        this.quality++;
+    } else if (this.sellIn <= 10 && this.sellIn > 5) {
+        this.quality = this.quality + 2;
+    } else if (this.sellIn <=5 && this.sellIn > 0) {
+        this.quality = this.quality + 3;
+    } else {
+        this.quality = 0;
     }
   }
 };
